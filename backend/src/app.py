5dae44a4f7a20d35c0d69da8721b212dd727bbda
g52332ar <andrew.robertson-2@student.manchester.ac.uk>
2023-10-28 14:32:30 +0100

    Removed Login Logic

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,31 +3,14 @@ import db
 
 
 app = Flask(__name__)
-session['loggedIn'] = False
-session['username'] = ""
-session['email'] = ""
+
 
 #Home Pages
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if (not (session.get('loggedIn'))):
-        return render_template('homePageNotLoggedIn.html')
     #Database Authentication for user
     return render_template('homePageLoggedIn.html')
-    session['loginAttempted'] = True
 
-
-#Log In / Registration
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if (session.get('loggedIn')):
-        return redirect(url_for('home'))
-    #Database Authentication for user
-    session['loginAttempted'] = True
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if (session.get('loggedIn')):
-        return redirect(url_for('home'))
-    #Database Authentication for user
-    session['loginAttempted'] = True
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('homePageLoggedIn.html')
