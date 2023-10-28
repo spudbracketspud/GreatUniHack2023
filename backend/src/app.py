45c0312738b68af4cd5b2ada64f4d7b75e2c3dba
g52332ar <andrew.robertson-2@student.manchester.ac.uk>
2023-10-28 14:09:04 +0100

    Rearranged app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,12 +3,13 @@ import db
 
 
 app = Flask(__name__)
+session['loggedIn'] = False
+session['username'] = ""
+session['email'] = ""
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if (not (session.get('loginAttempted'))):
-        session['loggedIn'] = False
-        session['username'] = ""
-        session['email'] = ""
-    
+    if (not (session.get('loggedIn'))):
+        return render_template('homePageNotLoggedIn.html')
+    #Database Authentication for user
     session['loginAttempted'] = True
