702333d3ef5cfd118928795eceb041d488f9624b
g52332ar <andrew.robertson-2@student.manchester.ac.uk>
2023-10-28 14:19:05 +0100

    Defined Login and Registration methods

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,9 +7,27 @@ session['loggedIn'] = False
 session['username'] = ""
 session['email'] = ""
 
+#Home Pages
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if (not (session.get('loggedIn'))):
         return render_template('homePageNotLoggedIn.html')
+    #Database Authentication for user
+    return render_template('homePageLoggedIn.html')
+    session['loginAttempted'] = True
+
+
+#Log In / Registration
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if (session.get('loggedIn')):
+        return redirect(url_for('home'))
+    #Database Authentication for user
+    session['loginAttempted'] = True
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if (session.get('loggedIn')):
+        return redirect(url_for('home'))
     #Database Authentication for user
     session['loginAttempted'] = True
