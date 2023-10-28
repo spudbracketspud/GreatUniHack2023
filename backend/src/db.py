d8ce019676075f5bff08357f8c436d2ee5919c3f
g52332ar <andrew.robertson-2@student.manchester.ac.uk>
2023-10-28 18:25:58 +0100

    Database, done things, adn stuff, Idk

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,18 +1,45 @@
-import bcrypt as bc
-import mysql.connector as sr
+import mysql.connector as mc
 
-def createDBs():
-  
-  try:
-      conn = sr.connect(host='localhost', user='root', database='logAlog')
-  except:
-      try:
-          conn = sr.connect(host='localhost', user='root',
-                            password="root", database='logAlog')
-      except:
-          conn = sr.connect(host='localhost', user='root',
-                            database='logAlog', password='rootroot')
-  cur = conn.cursor()
-  cur.execute("CREATE database IF NOT EXISTS logAlog")
-  cur.execute("USE logAlog")
+def createDB():
+    try:
+        conn = mc.connect(host='localhost', user='root', password='rootroot')
+        cur = conn.cursor()
+        cur.execute("CREATE DATABASE IF NOT EXISTS logAlog")
+        conn.commit()
+        conn.close()
+    except mc.Error as e:
+        print(f"Error creating database: {e}")
+
+def createTable():
+    try:
+        conn = mc.connect(host='localhost', user='root', password='rootroot', database='logAlog')
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS amazonImg (idpic INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, capture LONGBLOB NOT NULL)")
+        conn.commit()
+        conn.close()
+    except mc.Error as e:
+        print(f"Error creating table: {e}")
+
+def insertData():
+    try:
+        conn = mc.connect(host='localhost', user='root', password='rootroot', database='logAlog')
+        cur = conn.cursor()
+        insertQuery = "INSERT INTO amazonImg (capture) VALUES (%s)"
+        data = (convertToBinaryData("greenCubeTest.jpeg"),)
+        cur.execute(insertQuery, data)
+        conn.commit()
+        conn.close()
+    except mc.Error as e:
+        print(f"Error inserting data: {e}")
+
+# Call the functions
+createDB()
+createTable()
+insertData()
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        blobData = file.read()
+    return blobData
   
