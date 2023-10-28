e6b82ec2e562ffc3d4be465ad40ed0306c2e9c4e
Mary Kassayova <mkassayova39@gmail.com>
2023-10-28 20:31:35 +0100

    Front page done

---
+++ frontend/src/components/container.tsx
@@ -0,0 +1,10 @@
+
+const Container = ({ children }: any) => {
+  return(
+    <div className="mt-16 bg-seasalt w-full h-full">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/container";
 import NavBar from "../components/navbar";
 
 
@@ -5,8 +6,14 @@ function Homepage() {
   return (
     <div className="fixed h-full w-full min-h-screen bg-seasalt">
       <NavBar />
-      {/* <div className="flex flex-col absolute w-full h-full left-80 pr-80 pb-16 border-l-[1px] bg-jade">
-      </div> */}
+      <Container>
+        <div className="flex flex-col space-y-28 text-center">
+          <h1 className="pt-1/3 font-display text-5xl font-semibold mt-60">Welcome to Log-A-Log!</h1>
+
+          {/* TODO add text here */}
+          <h2 className="font-body text-xl w-2/3 m-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit, turpis eget accumsan posuere, nisi justo eleifend eros, eu eleifend felis odio vel ipsum. Quisque nec erat a nunc bibendum hendrerit. Vivamus vestibulum vel dui non fringilla. Fusce vel lacinia justo. Nullam at pulvinar tellus. Sed ultrices ligula in dolor efficitur, sed tempor mi cursus. Donec auctor, metus at malesuada sagittis, velit erat fringilla augue, nec tristique nunc justo et lectus.</h2>
+        </div>
+      </Container>
     </div>
 
   );
