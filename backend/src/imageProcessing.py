e126277b28180f14f9dc6b4f9f92ebe05e8db042
g52332ar <andrew.robertson-2@student.manchester.ac.uk>
2023-10-29 11:02:44 +0000

    Image processing optimisation

--- backend/src/imageProcessing.py
+++ backend/src/imageProcessing.py
@@ -7,7 +7,7 @@ def process(path):
         image = cv2.GaussianBlur(image, (0, 0), 10)
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        lower_green = np.array([35, 50, 50])  # Adjust these values based on your specific green color
+        lower_green = np.array([35, 50, 50]) 
         upper_green = np.array([85, 255, 255])
 
         green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
