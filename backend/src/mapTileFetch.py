a905dd6407dd8d74f0e79debe1e44b771c1e1204
m65100ar <aidan.read@student.manchester.ac.uk>
2023-10-29 10:23:49 +0000

    remove api key lol

--- backend/src/mapTileFetch.py
+++ backend/src/mapTileFetch.py
@@ -6,7 +6,7 @@ import urllib.request
 import ssl
 import math
 
-API_KEY = "AIzaSyD7lLYDxML-ngabV_hdr2oM3-V6tpvgxt8"
+API_KEY = input()
 VIEWPORT_BASE_URL = "https://tile.googleapis.com/tile/v1/viewport"
 TILES_BASE_URL = "https://tile.googleapis.com/v1/2dtiles"  
 SESSION_BASE_URL = "https://tile.googleapis.com/v1/createSession"
@@ -39,7 +39,7 @@ def make_request(url,data=None,json_key=None):
         else:
             # if no IOError, parse result
             if response.status == 200:
-                # if no errors, return the list of tiles
+                # if no errors, return the response json
                 if json_key == None:
                     return
                 result = json.load(response)
@@ -52,6 +52,35 @@ def make_request(url,data=None,json_key=None):
 
         time.sleep(current_delay)
         current_delay *= 2  # increase the delay each time we retry
+
+
+def fetch_image(url,path,filename):
+    print(url)
+    path = f"{path}/{filename}"
+    current_delay = 0.1 # set initial retry delay to 100ms
+    max_delay = 3 # set max retry delay to 5s
+
+    while True:
+        try:
+            response = urllib.request.urlopen(url,context=SSL_CONTEXT)
+        except urllib.error.URLError as error:
+            print(error)
+            pass
+        else:
+            if response.status == 200:
+                img = response.read()
+                with open(path,"wb") as handler:
+                    handler.write(img)
+                return
+
+        if current_delay > max_delay:
+                raise Exception("Too many retry attempts")
+
+        print("Waiting", current_delay, "seconds before retrying.")
+
+        time.sleep(current_delay)
+        current_delay *= 2  # increase the delay each time we retry
+
 
 """
 Fetches a token for making requests, estabishes for the tile types
@@ -106,21 +135,22 @@ def fetch_map_tile_bounds(API_KEY,SESSION_TOKEN,lat_bound,lon_bound,zoom):
     return make_request(url,json_key="maxZoomRects")
 
 def fetch_map_tile_images(API_KEY,SESSION_TOKEN,lat_bound,lon_bound,zoom):
-    lat = (lat_bound[0] + lat_bound[1]) / 2
-    lon = (lon_bound[0] + lon_bound[1]) / 2
+    tile_x0, tile_y0 = lat_lon_to_tile(lat_bound[0],lon_bound[0],zoom)
+    tile_x1, tile_y1 = lat_lon_to_tile(lat_bound[1],lon_bound[1],zoom)
 
-    tile_x, tile_y = lat_lon_to_tile(lat,lon,zoom)
+    for i in range(tile_y0,tile_y1+1):
+        for j in range(tile_y0,tile_y1+1):
 
-    params = urllib.parse.urlencode(
-        {
-        "session":SESSION_TOKEN,"key":API_KEY,
-        }
-    )
 
-    url = f"{TILES_BASE_URL}/{zoom}/{tile_x}/{tile_y}?{params}"
-    print(url)
+            params = urllib.parse.urlencode(
+                {
+                "session":SESSION_TOKEN,"key":API_KEY,
+                }
+            )
 
-    make_request(url)
+            url = f"{TILES_BASE_URL}/{zoom}/{i}/{j}?{params}"
+            name = f"test{i}{j}"
+            fetch_image(url,"captures",name)
 """
 given a bounded rectangle by latitude and longitude
 and the max zoom on that area
@@ -130,8 +160,11 @@ def zoom_out(lat_bound,lon_bound,zoom):
     tile_x0, tile_y0 = lat_lon_to_tile(lat_bound[0],lon_bound[0],zoom)
     tile_x1, tile_y1 = lat_lon_to_tile(lat_bound[1],lon_bound[1],zoom)
 
-    tile_xgap = abs(tile_x1 - tile_x0)
-    tile_ygap = abs(tile_y1 -tile_y0)
+    print([tile_x0,tile_y0])
+    print([tile_x1,tile_y1])
+
+    tile_xgap = abs(tile_x1 - tile_x0) + 1
+    tile_ygap = abs(tile_y1 -tile_y0) + 1
 
     print([tile_xgap,tile_ygap])
 
@@ -141,7 +174,7 @@ def zoom_out(lat_bound,lon_bound,zoom):
 
 def lat_lon_to_tile(lat,lon,zoom):
     lat_rad = (lat*math.pi) / 180
-    if lat_rad == 0:
+    if math.sin(lat_rad) == 0:
         lat_rad += 0.0001
 
     n = 2 **(zoom)
@@ -151,16 +184,18 @@ def lat_lon_to_tile(lat,lon,zoom):
     return tile_x,tile_y
 
 SESSION_TOKEN = fetch_session_token(API_KEY,"satellite")
-lat = (40,50)
-lon = (20,26)
+lat = (40,40.5)
+lon = (20,20.5)
 
 """obtain maximum zoom possible on latitude-longitude area
 want the last item in the list as that gives the smallest area"""
-map_tile_coords = fetch_map_tile_bounds(API_KEY,SESSION_TOKEN,lat,lon,22)
+map_tile_coords = fetch_map_tile_bounds(API_KEY,SESSION_TOKEN,lat,lon,10)
+print(map_tile_coords)
 max_zoom = map_tile_coords[-1]["maxZoom"]
+fetch_map_tile_images(API_KEY,SESSION_TOKEN,lat,lon,max_zoom)
 
 #determine whether this needs to be zoomed out to cover the whole area
-ideal_zoom = zoom_out(lat,lon,max_zoom)
-zoom = min(max_zoom,ideal_zoom)
+# ideal_zoom = zoom_out(lat,lon,max_zoom)
+# print([max_zoom,ideal_zoom])
+# zoom = min(max_zoom,ideal_zoom)
 
-print(fetch_map_tile_images(API_KEY,SESSION_TOKEN,lat,lon,zoom))
